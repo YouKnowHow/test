38df0f909d6e10ae651033e22d4adfd34f54fcbe
Radek <gordii@gazea.pl>
2015-09-12 11:55:55 +0200

    przesuniecie przycisku

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -55,7 +55,8 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(413, 12);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(437, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
